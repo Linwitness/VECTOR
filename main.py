f66068faa99d4689b23d1f752725d3a06e582e8a
Linwitness <linyangjump@outlook.com>
2021-10-05 18:23:35 -0400

    updated the main.py to output the final result

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import sys
 sys.path.append(current_path)
 import numpy as np
 import math
+from itertools import repeat
 
 import myInput
 import PACKAGE_MP_Vertex       #2D Vertex smooth algorithm
@@ -35,7 +36,8 @@ import PACKAGE_MP_3DLevelSet   #3D Level Set smooth algorithm
 # Validation Dream3d 831 grains sample ("s1400poly1_t0.init") with 0 timestep
 nx, ny, nz = 201, 201, 43
 ng = 831
-P0,R=myInput.init2IC3d(nx,ny,nz,ng,"s1400poly1_t0.init",True)
+filename = "s1400poly1_t0.init"
+P0,R=myInput.init2IC3d(nx,ny,nz,ng,filename,True)
 
 
 
@@ -80,5 +82,66 @@ for cores in [8]:
         
 #%% Output the inclination data
 
+def get_gb_sites(P,grain_num):
+    _,nx,ny,nz=np.shape(P)
+    timestep=5
+    ggn_gbsites = [[] for i in repeat(None, grain_num)]
+    for i in range(timestep,nx-timestep):
+        for j in range(timestep,ny-timestep):
+            for k in range(timestep,nz-timestep):
+                ip,im,jp,jm,kp,km = myInput.periodic_bc3d(nx,ny,nz,i,j,k)
+                if ( ((P[0,ip,j,k]-P[0,i,j,k])!=0) or ((P[0,im,j,k]-P[0,i,j,k])!=0) or\
+                     ((P[0,i,jp,k]-P[0,i,j,k])!=0) or ((P[0,i,jm,k]-P[0,i,j,k])!=0) or\
+                     ((P[0,i,j,kp]-P[0,i,j,k])!=0) or ((P[0,i,j,km]-P[0,i,j,k])!=0) ) and\
+                    P[0,i,j,k] <= grain_num:
+                    ggn_gbsites[int(P[0,i,j,k]-1)].append([i,j,k])
+    return ggn_gbsites
 
-          
+def norm_list(grain_num, P_matrix):
+    # get the norm list
+    grain_num -= 1
+    boundary_site = get_gb_sites(P_matrix, grain_num)
+    norm_list = [np.zeros(( len(boundary_site[i]), 3 )) for i in range(grain_num)]
+    for grain_i in range(grain_num):
+        print(f"finish {grain_i}")
+        
+        for site in range(len(boundary_site[grain_i])):
+            norm = myInput.get_grad3d(P_matrix, boundary_site[grain_i][site][0], boundary_site[grain_i][site][1], boundary_site[grain_i][site][2])
+            norm_list[grain_i][site,:] = list(norm)
+        
+    return norm_list, boundary_site
+
+def get_orientation(grain_num, init_name ):
+    # read the input euler angle from *.init
+    eulerAngle = np.ones((grain_num,3))*-10
+    with open('Input/'+init_name, 'r', encoding = 'utf-8') as f:
+        for line in f:
+            eachline = line.split()
+    
+            if len(eachline) == 5 and eachline[0] != '#':
+                lineN = int(eachline[1])-1
+                if eulerAngle[lineN,0] == -10:
+                    eulerAngle[lineN,:] = [float(eachline[2]), float(eachline[3]), float(eachline[4])]
+    return eulerAngle[:ng-1]
+
+def output(output_name, norm_list, site_list, orientation_list):
+    
+    file = open('output/'+output_name,'w')
+    for i in range(len(norm_list)):
+        file.writelines(['Grain ' + str(i+1) + ' Orientation: ' + str(orientation_list[i]) + ' centroid: ' + '\n'])
+        
+        for j in range(len(norm_list[i])):
+            file.writelines([str(site_list[i][j][0]) + ', ' + str(site_list[i][j][1]) + ', ' + str(site_list[i][j][2]) + ', ' + str(norm_list[i][j][0]) + ', ' + str(norm_list[i][j][1]) + ', ' + str(norm_list[i][j][2]) + '\n'])
+            
+        file.writelines(['\n'])
+    
+    file.close()
+    return
+
+norm_list1, site_list1 = norm_list(ng, P)
+orientation_list1 = get_orientation(ng, filename)
+output("total_site.txt", norm_list1, site_list1, orientation_list1)
+
+
+
+
