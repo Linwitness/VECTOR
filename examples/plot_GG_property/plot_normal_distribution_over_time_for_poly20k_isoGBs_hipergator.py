74355a91fa5f53ff41354770164c442bb98b20cf
Lin <linyangjump@outlook.com>
2023-09-21 12:59:35 -0400

    maginitude

--- examples/plot_GG_property/plot_normal_distribution_over_time_for_poly20k_isoGBs_hipergator.py
+++ examples/plot_GG_property/plot_normal_distribution_over_time_for_poly20k_isoGBs_hipergator.py
@@ -490,7 +490,7 @@ if __name__ == '__main__':
     plt.ylabel("Magnitude", fontsize=14)
     plt.xticks([0,1,2,3,4,5],label_list)
     plt.legend(fontsize=14)
-    plt.ylim([0.0,1.0])
+    plt.ylim([-0.05,1.1])
     plt.savefig(current_path + "/figures/anisotropic_poly_20k_isoGBs_magnitude_polar_ave.png", dpi=400,bbox_inches='tight')
 
 
