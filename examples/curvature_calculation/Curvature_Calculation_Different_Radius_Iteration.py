9536a7fe3a6f515f498b5018ede42be9f4f9690e
Linwitness <linyangjump@outlook.com>
2024-04-03 16:13:51 -0400

    curvature update

--- examples/curvature_calculation/Curvature_Calculation_Different_Radius_Iteration.py
+++ examples/curvature_calculation/Curvature_Calculation_Different_Radius_Iteration.py
@@ -27,7 +27,7 @@ def test_2d():
     max_iteration = 20
     radius = 20
     filename_save = f"examples/curvature_calculation/BL_Curvature_R{radius}_Iteration_1_{max_iteration}"
-    
+
     BL_errors =np.zeros(max_iteration)
     BL_runningTime = np.zeros(max_iteration)
 
@@ -59,7 +59,7 @@ def test_2d():
 
             BL_errors[loop_times-1] = test1.errors_per_site
             BL_runningTime[loop_times-1] = test1.running_coreTime
-            
+
     np.savez(filename_save, BL_errors=BL_errors, BL_runningTime=BL_runningTime)
 
 def test_vertex_2d():
@@ -69,7 +69,7 @@ def test_vertex_2d():
     max_iteration = 20
     radius = 80
     filename_save = f"./VT_Curvature_R{radius}_Iteration_1_{max_iteration}"
-    
+
     VT_errors =np.zeros(max_iteration)
     VT_runningTime = np.zeros(max_iteration)
 
@@ -101,52 +101,52 @@ def test_vertex_2d():
 
             VT_errors[interval-1] = test1.errors_per_site
             VT_runningTime[interval-1] = test1.running_coreTime
-            
+
     np.savez(filename_save, VT_errors=VT_errors, VT_runningTime=VT_runningTime)
 
 
 def test_3d():
     nx, ny, nz = 200, 200, 200
     ng = 2
-    cores = 8
+    cores = 16
     max_iteration = 20
-    radius = [5, 20, 50,80]
-    
+    radius = [80]
+
     for r in radius:
         filename_save = f"./BL3D_Curvature_R{r}_Iteration_1_{max_iteration}"
-        
+
         BL_errors =np.zeros(max_iteration)
         BL_runningTime = np.zeros(max_iteration)
-    
+
         # P0,R=myInput.init2IC(nx, ny, ng, "PolyIC.init")
         P0,R=myInput.Circle_IC3d(nx,ny,nz,r)
         # P0,R=myInput.Voronoi_IC(nx,ny,ng)
-    
+
         for cores in [cores]:
             # loop_times=10
             for loop_times in range(1,max_iteration):
-    
-    
+
+
                 test1 = Linear_3D.linear3d_class(nx,ny,nz,ng,cores,loop_times,P0,R,'np')
                 # test1.linear_main()
                 # P = test1.get_P()
-    
+
                 test1.linear3d_main("curvature")
                 C_ln = test1.get_C()
-    
+
                 # error
-    
+
                 print('loop_times = ' + str(test1.loop_times))
                 print('running_time = %.2f' % test1.running_time)
                 print('running_core time = %.2f' % test1.running_coreTime)
                 print('total_errors = %.2f' % test1.errors)
                 print('per_errors = %.3f' % test1.errors_per_site)
                 print()
-    
-    
+
+
                 BL_errors[loop_times-1] = test1.errors_per_site
                 BL_runningTime[loop_times-1] = test1.running_coreTime
-                
+
         np.savez(filename_save, BL_errors=BL_errors, BL_runningTime=BL_runningTime)
 
 def test_vertex_3d():
@@ -155,94 +155,94 @@ def test_vertex_3d():
     cores = 8
     max_iteration = 20
     radius = [5, 20, 50, 80, 2, 1]
-    
+
     for r in radius:
         filename_save = f"./VT3D_Curvature_R{r}_Iteration_1_{max_iteration}"
-        
+
         VT_errors =np.zeros(max_iteration)
         VT_runningTime = np.zeros(max_iteration)
-    
+
         # P0,R=myInput.init2IC(nx, ny, ng, "PolyIC.init")
         P0,R=myInput.Circle_IC3d(nx,ny,nz,r)
         # P0,R=myInput.Voronoi_IC(nx,ny,ng)
-    
+
         for cores in [cores]:
             # loop_times=10
             for interval in range(1,max_iteration):
-    
-    
+
+
                 test1 = Vertex_3D.vertex3d_class(nx,ny,nz,ng,cores,interval,P0,R)
                 # test1.linear_main()
                 # P = test1.get_P()
-    
+
                 test1.vertex3d_main("curvature")
                 C_ln = test1.get_C()
-    
+
                 # error
-    
+
                 print('loop_times = ' + str(test1.interval))
                 print('running_time = %.2f' % test1.running_time)
                 print('running_core time = %.2f' % test1.running_coreTime)
                 print('total_errors = %.2f' % test1.errors)
                 print('per_errors = %.3f' % test1.errors_per_site)
                 print()
-    
-    
+
+
                 VT_errors[interval-1] = test1.errors_per_site
                 VT_runningTime[interval-1] = test1.running_coreTime
-                
+
         np.savez(filename_save, VT_errors=VT_errors, VT_runningTime=VT_runningTime)
-        
+
 def test_vertex_3dComplex():
     nx, ny, nz = 200, 200, 200
     ng = 2
     cores = 8
     max_iteration = 20
     wave = [5, 20, 50, 80, 2, 1]
-    
+
     for w in wave:
         filename_save = f"./VT3DComp_Curvature_wave{w}_Iteration_1_{max_iteration}"
-        
+
         VT_errors =np.zeros(max_iteration)
         VT_runningTime = np.zeros(max_iteration)
-    
+
         # P0,R=myInput.init2IC(nx, ny, ng, "PolyIC.init")
         # P0,R=myInput.Circle_IC3d(nx,ny,nz,r)
         P0,R=myInput.Complex2G_IC3d(nx,ny,nz,w)
         # P0,R=myInput.Voronoi_IC(nx,ny,ng)
-    
+
         for cores in [cores]:
             # loop_times=10
             for interval in range(1,max_iteration):
-    
-    
+
+
                 test1 = Vertex_3D.vertex3d_class(nx,ny,nz,ng,cores,interval,P0,R)
                 # test1.linear_main()
                 # P = test1.get_P()
-    
+
                 test1.vertex3d_main("curvature")
                 C_ln = test1.get_C()
-    
+
                 # error
-    
+
                 print('loop_times = ' + str(test1.interval))
                 print('running_time = %.2f' % test1.running_time)
                 print('running_core time = %.2f' % test1.running_coreTime)
                 print('total_errors = %.2f' % test1.errors)
                 print('per_errors = %.3f' % test1.errors_per_site)
                 print()
-    
-    
+
+
                 VT_errors[interval-1] = test1.errors_per_site
                 VT_runningTime[interval-1] = test1.running_coreTime
-                
+
         np.savez(filename_save, VT_errors=VT_errors, VT_runningTime=VT_runningTime)
 
 
 
 
 if __name__ == '__main__':
-    
-    test_vertex_3dComplex()
-    
-    
+
+    # test_vertex_3dComplex()
+    test_3d()
+
--- examples/curvature_calculation/Curvature_Comparison.py
+++ examples/curvature_calculation/Curvature_Comparison.py
@@ -17,21 +17,21 @@ import matplotlib.pyplot as plt
 
 def plot_test2D():
     data_num = 19
-    
+
     file1 = 'BL_Curvature_R5_Iteration_1_20.npz'
     file2 = 'BL_Curvature_R20_Iteration_1_20.npz'
     file3 = 'BL_Curvature_R50_Iteration_1_20.npz'
     file4 = 'BL_Curvature_R80_Iteration_1_20.npz'
     function_name = 'BL_errors'
-    
+
     r5 = np.load(file1)
     r20 = np.load(file2)
     r50 = np.load(file3)
     r80 = np.load(file4)
-    
+
     fig = plt.figure()
     # ax = fig.add_subplot(1, 1, 1)
-    
+
     plt.plot(range(data_num),(r5[function_name][:data_num])/(1/5), label = "r=05")
     plt.plot(range(data_num),(r20[function_name][:data_num])/(1/20),label = "r=20")
     plt.plot(range(data_num),(r50[function_name][:data_num])/(1/50),label = "r=50")
@@ -41,24 +41,24 @@ def plot_test2D():
     plt.legend()
     plt.ylim([0,6])
     plt.xlim([-0.1,20])
-    
+
 def plot_VT_test2D():
     data_num = 19
-    
+
     file1 = 'VT_Curvature_R5_Iteration_1_20.npz'
     file2 = 'VT_Curvature_R20_Iteration_1_20.npz'
     file3 = 'VT_Curvature_R50_Iteration_1_20.npz'
     file4 = 'VT_Curvature_R80_Iteration_1_20.npz'
     function_name = 'VT_errors'
-    
+
     r5 = np.load(file1)
     r20 = np.load(file2)
     r50 = np.load(file3)
     r80 = np.load(file4)
-    
+
     fig = plt.figure()
     # ax = fig.add_subplot(1, 1, 1)
-    
+
     plt.plot(range(data_num),(r5[function_name][:data_num])/(1/5), label = "r=05")
     plt.plot(range(data_num),(r20[function_name][:data_num])/(1/20),label = "r=20")
     plt.plot(range(data_num),(r50[function_name][:data_num])/(1/50),label = "r=50")
@@ -68,39 +68,40 @@ def plot_VT_test2D():
     plt.legend()
     plt.ylim([0,6])
     plt.xlim([-0.1,20])
-    
+
 def plot_test3D():
     data_num = 19
-    
+
     file1 = 'BL3D_Curvature_R5_Iteration_1_20.npz'
     file2 = 'BL3D_Curvature_R20_Iteration_1_20.npz'
     file3 = 'BL3D_Curvature_R50_Iteration_1_20.npz'
-    # file4 = 'BL_Curvature_R80_Iteration_1_20.npz'
+    file4 = 'BL_Curvature_R80_Iteration_1_20.npz'
     function_name = 'BL_errors'
-    
+
     r5 = np.load(file1)
     r20 = np.load(file2)
     r50 = np.load(file3)
-    # r80 = np.load(file4)
-    
+    r80 = np.load(file4)
+
     fig = plt.figure()
     # ax = fig.add_subplot(1, 1, 1)
-    
+
     plt.plot(range(data_num),(r5[function_name][:data_num])/(1/5), label = "r=05", color="royalblue")
     plt.plot(range(data_num),(r20[function_name][:data_num])/(1/20),label = "r=20", color="orange")
     plt.plot(range(data_num),(r50[function_name][:data_num])/(1/50),label = "r=50", color="green")
-    # plt.plot(range(data_num),(r80[function_name][:data_num])/(1/80),label = "r=80")
+    plt.plot(range(data_num),(r80[function_name][:data_num])/(1/80),label = "r=80", color="purple")
     plt.plot([-0.1,data_num],[abs(r5_vv-0.2)/0.2,abs(r5_vv-0.2)/0.2],'--', color="royalblue")
     plt.plot([-0.1,data_num],[abs(r20_vv-0.05)/0.05,abs(r20_vv-0.05)/0.05],'--', color="orange")
     plt.plot([-0.1,data_num],[abs(r50_vv-0.02)/0.02,abs(r50_vv-0.02)/0.02],'--', color="green")
+    plt.plot([-0.1,data_num],[abs(r80_vv-0.0125)/0.0125,abs(r80_vv-0.0125)/0.0125],'--', color="purple")
     plt.legend()
     plt.yscale('log')
     plt.ylim([0,2])
     plt.xlim([-0.1,20])
-    
+
 def plot_VT_test3D():
     data_num = 19
-    
+
     file1 = 'VT3D_Curvature_R5_Iteration_1_20.npz'
     file2 = 'VT3D_Curvature_R20_Iteration_1_20.npz'
     file3 = 'VT3D_Curvature_R50_Iteration_1_20.npz'
@@ -108,17 +109,17 @@ def plot_VT_test3D():
     file5 = 'VT3D_Curvature_R2_Iteration_1_20.npz'
     file6 = 'VT3D_Curvature_R1_Iteration_1_20.npz'
     function_name = 'VT_errors'
-    
+
     r5 = np.load(file1)
     r20 = np.load(file2)
     r50 = np.load(file3)
     r80 = np.load(file4)
     r2 = np.load(file5)
     r1 = np.load(file6)
-    
+
     fig = plt.figure()
     # ax = fig.add_subplot(1, 1, 1)
-    
+
     plt.plot(range(data_num),(r5[function_name][:data_num])/(1/5), label = "r=05", color="royalblue")
     plt.plot(range(data_num),(r20[function_name][:data_num])/(1/20),label = "r=20", color="orange")
     plt.plot(range(data_num),(r50[function_name][:data_num])/(1/50),label = "r=50", color="green")
@@ -131,13 +132,13 @@ def plot_VT_test3D():
     plt.plot([-0.1,data_num],[abs(r80_vv-0.0125)/0.0125,abs(r80_vv-0.0125)/0.0125],'--', color="purple")
     plt.plot([-0.1,data_num],[abs(r2_vv-0.5)/0.5,abs(r2_vv-0.5)/0.5],'--', color="red")
     plt.plot([-0.1,data_num],[abs(r1_vv-1)/1,abs(r1_vv-1)/1],'--', color="gray")
-    
+
     plt.legend()
     plt.yscale('log')
     plt.ylim([0,200])
     plt.xlim([-0.1,20])
-    
-    
+
+
 r1_vv = 1.570796333
 r2_vv = 0.523598778
 r5_vv = 0.204886473
